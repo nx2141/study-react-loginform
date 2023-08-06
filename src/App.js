0c79224147afe879d79993a750310345c0054303
nx2141 <super_red_block@yahoo.co.jp>
2023-08-07 00:57:36 +0900

    ✨ できました

--- src/App.js
+++ src/App.js
@@ -9,9 +9,42 @@ function App() {
     password: "",
   };
   const [formValues, setformValues] = useState(initialValues);
+  const [formErrors, setformErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
+    const { name, value } = e.target;
+    setformValues({ ...formValues, [name]: value });
+    console.log(formValues);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //ログイン情報を送信する
+    //バリデーションチェックをする
+    setformErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+  const validate = (values) => {
+    const errors = {};
+    const regex =
+      /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+    if (!values.username) {
+      errors.username = "ユーザー名を入力してください";
+    }
+    if (!values.mailAddress) {
+      errors.mailAddress = "メールアドレスを入力してください";
+    } else if (!regex.test(values.mailAddress)) {
+      errors.mailAddress = "正しいメールアドレスを入力してください";
+    }
+    if (!values.password) {
+      errors.password = "パスワードを入力してください";
+    } else if (values.password.length < 4) {
+      errors.password = "4文字以上15文字以下のパスワードを入力してください";
+    } else if (values.password.length > 15) {
+      errors.password = "4文字以上15文字以下のパスワードを入力してください";
+    }
+    return errors;
   };
   return (
     <div className="formContainer">
@@ -27,15 +60,33 @@ function App() {
             onChange={handleChange}
           />
         </div>
+        <p className="errorMsg">{formErrors.username}</p>
         <div className="formField">
           <label>メールアドレス</label>
-          <input type="text" placeholder="メールアドレス" name="mailAddress" />
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="メールアドレス"
+            name="mailAddress"
+          />
         </div>
+        <p className="errorMsg">{formErrors.mailAddress}</p>
         <div className="formField">
           <label>パスワード</label>
-          <input type="text" placeholder="パスワード" name="password" />
+          <input
+            onChange={handleChange}
+            type="text"
+            placeholder="パスワード"
+            name="password"
+          />
         </div>
-        <button className="submitButton">ログイン</button>
+        <p className="errorMsg">{formErrors.password}</p>
+        <button onClick={handleSubmit} className="submitButton">
+          ログイン
+        </button>
+        {Object.keys(formErrors).length === 0 && isSubmit && (
+          <div className="msgOk">ログイン成功です</div>
+        )}
       </form>
     </div>
   );
